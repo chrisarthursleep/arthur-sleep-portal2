38fc5ff8e98232780afe80f1fa75f2166acc021e
chrisarthursleep <chris@arthursleep.com>
2025-06-25 15:28:25 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,9 @@
-// Re-export all types for convenient importing
 export * from './customer'
 export * from './order'
-export * from './team'
 export * from './wardrobe'
 export * from './navigation'
 export * from './weather'
-export * from './message'
+
+// Selective exports to avoid name clash
+export { TeamMember, MessageAttachment as TeamMessageAttachment } from './team'
+export { Message, MessageStatus, MessageAttachment as ChatMessageAttachment } from './message'
